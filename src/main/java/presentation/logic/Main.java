f00156ba7d262d5d4d6cb44bd8e6768cfb9dcf34
DaveNavarro20 <153691441+DaveNavarro20@users.noreply.github.com>
2025-08-17 23:06:46 -0600

    Initial commit

---
+++ src/main/java/presentation/logic/Main.java
@@ -0,0 +1,22 @@
+package presentation.logic;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        }catch (Exception e){};
+
+        View_Loggin view =  new View_Loggin();
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,300);
+        frame.setTitle("Clinica");
+        frame.setContentPane(view.getPanelLoggin());
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/main/java/presentation/logic/View_CambiarClave.java
@@ -0,0 +1,17 @@
+package presentation.logic;
+
+import javax.swing.*;
+
+public class View_CambiarClave {
+    private JButton button3;
+    private JButton botonInicioSesion;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
+    private JPanel panelCambiarCleve;
+
+
+    public JPanel getPanelCambiarClave(){
+        return panelCambiarCleve;
+    }
+}
