9d8d439f54e97ed13bd68ece2ebab41b12ef6e7a
DaveNavarro20 <153691441+DaveNavarro20@users.noreply.github.com>
2025-08-18 22:50:56 -0600

    View primera ventada

---
+++ src/main/java/presentation/logic/View_Admin.java
@@ -0,0 +1,21 @@
+package presentation.logic;
+
+import javax.swing.*;
+
+public class View_Admin {
+    private JPanel panel;
+    private JLabel searchNombreLbl;
+    private JTextField searchNombre;
+    private JButton report;
+    private JButton search;
+    private JTable list;
+    private JLabel idLbl;
+    private JTextField IdJtext;
+    private JLabel EspecialidadLbl;
+    private JTextField EspecialidadJtext;
+    private JLabel NombreLbl;
+    private JTextField NombreJtext;
+    private JButton save;
+    private JButton delete;
+    private JButton clear;
+}
