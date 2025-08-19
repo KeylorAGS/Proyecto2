c62ec24a6f48991d3fd5f284ecf555c5ed26fe85
keneth023 <kenethj2303@gmail.com>
2025-08-19 12:39:05 -0600

    a

---
+++ src/main/java/presentation/logic/View_Prescripcion.java
@@ -0,0 +1,22 @@
+package presentation.logic;
+
+import javax.swing.*;
+
+public class View_Prescripcion {
+    private JPanel panel1;
+    private JPanel panel;
+    private JPanel control;
+    private JButton buscarPaciente;
+    private JButton agregarMedicamento;
+    private JTextField fechaRetiroT;
+    private JButton fechaRetiroB;
+    private JLabel paciente;
+    private JLabel fechaRetiro;
+    private JTable table1;
+    private JPanel ajustarPrescripcion;
+    private JButton guardar;
+    private JButton descartarMedicamentoButton;
+    private JButton detallesButton;
+    private JButton limpiarButton;
+
+}
