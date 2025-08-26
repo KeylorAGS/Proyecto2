e6611c648beecbf1788a4105b9b338aa64041220
DaveNavarro20 <153691441+DaveNavarro20@users.noreply.github.com>
2025-08-26 01:56:06 -0600

    Create PacientesTableModel.java

---
+++ src/main/java/presentation/Pacientes/PacientesTableModel.java
@@ -0,0 +1,43 @@
+package presentation.Pacientes;
+
+import presentation.AbstractTableModel;
+import presentation.Logic.Paciente;
+
+import javax.swing.table.TableModel;
+import java.util.List;
+
+public class PacientesTableModel extends AbstractTableModel<Paciente> implements TableModel {
+    public static final int ID = 0;
+    public static final int NOMBRE = 1;
+    public static final int FECHA_NACIMIENTO = 2;
+    public static final int TELEFONO = 3;
+
+    public PacientesTableModel(int[] cols, List<Paciente> rows) {
+        super(cols, rows);
+    }
+
+    @Override
+    protected Object getPropetyAt(Paciente e, int col) {
+        switch (cols[col]) {
+            case ID:
+                return e.getId();
+            case NOMBRE:
+                return e.getNombre();
+            case FECHA_NACIMIENTO:
+                return e.getFechaNacimiento();
+            case TELEFONO:
+                return e.getTelefono();
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    protected void initColNames() {
+        colNames = new String[4];
+        colNames[ID] = "Id";
+        colNames[NOMBRE] = "Nombre";
+        colNames[FECHA_NACIMIENTO] = "Fecha Nacimiento";
+        colNames[TELEFONO] = "Teléfono";
+    }
+}
